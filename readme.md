3645bbecfe6c9b06a81886d9909c1af508d3ab14
Bruno Gomes <brunoo.gomes11@gmail.com>
2023-06-23 17:53:06 -0300

    [FEAT]: List, edit and delete a meal

--- readme.md
+++ readme.md
@@ -2,7 +2,7 @@
 [x] Deve ser possível identificar o usuário entre as requisições.
 [] O usuário só pode visualizar, editar e apagar as refeições o qual ele criou.
 
-[] Deve ser possível recuperar as métricas de um usuário
+[x] Deve ser possível recuperar as métricas de um usuário
     [x] - Quantidade total de refeições registradas
     [x] - Quantidade total de refeições dentro da dieta
     [x] - Quantidade total de refeições fora da dieta
@@ -14,9 +14,7 @@
     - Descrição
     - Data e Hora
     - Está dentro ou não da dieta
-[] Deve ser possível editar uma refeição, podendo alterar todos os dados acima.
-[] Deve ser possível apagar uma refeição.
-[] Deve ser possível listar todas as refeições de um usuário.
-[] Deve ser possível visualizar uma única refeição.
-
-
+[x] Deve ser possível editar uma refeição, podendo alterar todos os dados acima.
+[x] Deve ser possível apagar uma refeição.
+[x] Deve ser possível listar todas as refeições de um usuário.
+[x] Deve ser possível visualizar uma única refeição.
--- src/routes/refeicoes.ts
+++ src/routes/refeicoes.ts
@@ -10,24 +10,149 @@ export async function refeicaoRoutes(app: FastifyInstance) {
     const createRefeicaoBodySchema = z.object({
       nome: z.string(),
       descricao: z.string(),
-      data_hora: z.string(),
       esta_na_dieta: z.boolean(),
     })
     // eslint-disable-next-line dot-notation
     const userid = request.headers['userid']
 
-    const { nome, descricao, data_hora, esta_na_dieta } =
-      createRefeicaoBodySchema.parse(request.body)
+    const { nome, descricao, esta_na_dieta } = createRefeicaoBodySchema.parse(
+      request.body,
+    )
+    const now = new Date()
+    // Formata a data no formato 'dd/mm/yyyy hh:mm:ss'
+    const formattedDate = now.toLocaleString('pt-BR', {
+      day: '2-digit',
+      month: '2-digit',
+      year: 'numeric',
+      hour: '2-digit',
+      minute: '2-digit',
+      second: '2-digit',
+    })
 
     await knex('refeicao').insert({
       id: randomUUID(),
       nome,
       descricao,
-      data_hora,
+      data_hora: formattedDate,
       esta_na_dieta,
       user: userid,
     })
 
     return reply.status(201).send()
   })
+  // Listar todas as refeicoes
+  app.get('/', { preHandler: [checkUserIdExists] }, async (request, reply) => {
+    // eslint-disable-next-line dot-notation
+    const userid = request.headers['userid']
+
+    const meals = await knex('refeicao').where('user', userid).select('*')
+
+    return reply.status(200).send(meals)
+  })
+  // Listar uma unica refeicao
+  app.get(
+    '/:idRefeicao',
+    { preHandler: [checkUserIdExists] },
+    async (request, reply) => {
+      const { idRefeicao } = request.query
+      const userId = request.headers.userid
+
+      // Verifica se a refeição existe
+      const meal = await knex('refeicao').where('id', idRefeicao).select('*')
+      if (meal.length === 0) {
+        return reply.status(200).send({ meal: [] })
+      }
+      // Verifica se o usuário tem permissão para listar a refeição
+      const mealUserId = meal[0].user
+      if (mealUserId !== userId) {
+        return reply.status(406).send('Permission Denied, user not permitted.')
+      }
+
+      return reply.status(200).send(meal)
+    },
+  )
+  // Deve ser possivel editar uma refeicao
+  app.patch(
+    '/:idRefeicao',
+    { preHandler: [checkUserIdExists] },
+    async (request, reply) => {
+      const createRefeicaoBodySchema = z.object({
+        nome: z.string(),
+        descricao: z.string(),
+        // data_hora: z.string(),
+        esta_na_dieta: z.boolean(),
+      })
+      try {
+        const userId = request.headers.userid
+        const { idRefeicao } = request.query
+
+        // Verifica se a refeição existe
+        const meal = await knex('refeicao').where('id', idRefeicao).select('*')
+        if (meal.length === 0) {
+          return reply.status(200).send({ meal: [] })
+        }
+        // Verifica se o usuário tem permissão para editar a refeição
+        const mealUserId = meal[0].user
+        if (mealUserId !== userId) {
+          return reply
+            .status(406)
+            .send('Permission Denied, user not permitted.')
+        }
+        const { nome, descricao, esta_na_dieta } =
+          createRefeicaoBodySchema.parse(request.body)
+        // alterar a refeição
+        const now = new Date()
+        // Formata a data no formato 'dd/mm/yyyy hh:mm:ss'
+        const formattedDate = now.toLocaleString('pt-BR', {
+          day: '2-digit',
+          month: '2-digit',
+          year: 'numeric',
+          hour: '2-digit',
+          minute: '2-digit',
+          second: '2-digit',
+        })
+        await knex('refeicao').where('id', idRefeicao).update({
+          nome,
+          descricao,
+          data_hora: formattedDate,
+          esta_na_dieta,
+        })
+
+        return reply.status(204).send()
+      } catch (error) {
+        // Handle error
+        return reply.status(500).send('Internal Server Error')
+      }
+    },
+  )
+  // Deve ser possivel apagar uma refeicao
+  app.put(
+    '/:idRefeicao',
+    { preHandler: [checkUserIdExists] },
+    async (request, reply) => {
+      try {
+        const userId = request.headers.userid
+        const { idRefeicao } = request.query
+
+        // Verifica se a refeição existe
+        const meal = await knex('refeicao').where('id', idRefeicao).select('*')
+        if (meal.length === 0) {
+          return reply.status(200).send({ meal: [] })
+        }
+        // Verifica se o usuário tem permissão para deletar a refeição
+        const mealUserId = meal[0].user
+        if (mealUserId !== userId) {
+          return reply
+            .status(406)
+            .send('Permission Denied, user not permitted.')
+        }
+        // Deleta a refeição
+        await knex('refeicao').where('id', idRefeicao).delete()
+        return reply.status(204).send()
+      } catch (error) {
+        // Handle error
+        return reply.status(500).send('Internal Server Error')
+      }
+    },
+  )
 }
